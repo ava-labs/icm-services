c09eb91c002fed3146630637c86b8f15b026fcdb
Geoff Stuart <geoffrey.stuart@avalabs.org>
2025-11-26 06:48:28 -0500

    consolidate isntall scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -35,7 +35,6 @@ function solLinter() {
 function golangLinter() {
     echo "Linting Golang code..."
     cd $REPO_PATH
-
     go run github.com/golangci/golangci-lint/cmd/golangci-lint run --config=$REPO_PATH/.golangci.yml --build-tags=test ./... --timeout 5m
     (cd proto && go run github.com/bufbuild/buf/cmd/buf lint)
 }
