5ee40f66c7c84b5208e02ce0b6ee699b2a741c2a
mdelle1 <108158289+mdelle1@users.noreply.github.com>
2026-01-09 14:26:09 -0500

    Work

--- icm-contracts/avalanche/teleporter/verifiers/tseth.sol
+++ icm-contracts/avalanche/teleporter/verifiers/tseth.sol
@@ -1,4 +1,12 @@
-// SPDX-License-Identifier: Apache-2.0
+// (c) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: LicenseRef-Ecosystem
+
+// Based on code from: https://github.com/boundless-xyz/boundless-transceiver
+// Modifications: Builds a State Manager for the Ethereum beacon chain, including storing beacon state roots, 
+// execution state roots, and receipt roots, on top of the infrastructure provided by the boundless-transceiver repository.
+
 pragma solidity ^0.8.30;
 
 struct Checkpoint {
