1848c98554792a31b52973fe5d112e87bd35a8f5
mdelle1 <108158289+mdelle1@users.noreply.github.com>
2026-01-13 15:36:40 -0500

    Add SimpleSerialize library

---
+++ icm-contracts/avalanche/utilities/SimpleSerialize.sol
@@ -0,0 +1,43 @@
+// (c) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: LicenseRef-Ecosystem
+
+// Reference: This is core logic from the Succinct Telepathy Library, which can be found at https://github.com/succinctlabs/telepathy-contracts/blob/main/src/libraries/SimpleSerialize.sol 
+pragma solidity ^0.8.30;
+
+library SSZ {
+    /// @notice Computes the Merkle Root using a leaf, its index, and the proof
+    /// @param leaf The leaf data to verify (e.g., beaconStateRoot)
+    /// @param index The Generalized Index (gIndex) of the leaf
+    /// @param proof The Merkle proof (array of sibling hashes)
+    function restoreMerkleRoot(bytes32 leaf, uint256 index, bytes32[] memory proof)
+        internal
+        pure
+        returns (bytes32)
+    {
+        bytes32 value = leaf;
+        uint256 i = 0;
+        while (index != 1) {
+            if (index % 2 == 1) {
+                // If index is odd, we are a Right child. Sibling is on the Left.
+                value = sha256(bytes.concat(proof[i], value));
+            } else {
+                // If index is even, we are a Left child. Sibling is on the Right.
+                value = sha256(bytes.concat(value, proof[i]));
+            }
+            index /= 2;
+            i++;
+        }
+        return value;
+    }
+
+    /// @notice Verifies that a Merkle proof correctly connects a leaf to a root
+    function isValidMerkleProof(bytes32 leaf, uint256 index, bytes32[] memory proof, bytes32 root)
+        internal
+        pure
+        returns (bool)
+    {
+        return restoreMerkleRoot(leaf, index, proof) == root;
+    }
+}
---
+++ icm-contracts/avalanche/utilities/tests/SimpleSerializeTests.t.sol
@@ -0,0 +1,26 @@
+// (c) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: LicenseRef-Ecosystem
+
+// Reference: This is core logic from the Succinct Telepathy Library, which can be found at https://github.com/succinctlabs/telepathy-contracts/blob/main/test/libraries/SimpleSerialize.t.sol
+pragma solidity 0.8.30;
+
+import {Test} from "@forge-std/Test.sol";
+import { SSZ } from "../SimpleSerialize.sol";
+
+contract SSZTest is Test {
+    function test_FinalityProof_WhenEthereum() public pure {
+        uint256 index = 105;
+        bytes32[] memory branch = new bytes32[](6);
+        branch[0] = bytes32(0xe424020000000000000000000000000000000000000000000000000000000000);
+        branch[1] = bytes32(0x75410a8f37f9506fb3f972cce6ece955e381e51037e432ce4ca47479c9cd9158);
+        branch[2] = bytes32(0xe6af38835c0ac3c2b0d561dfaec168171d7d77c1c2e8e74ff9b1891cf43faf8d);
+        branch[3] = bytes32(0x3e4fb2d12bd835bc6ee23b5ec65a43f4493e32f5ef45d46bd2c38830b17672bb);
+        branch[4] = bytes32(0x880548f4df2d4003f7be2fbbde112eb46b8f756b5e33202e04863000e4383f3b);
+        branch[5] = bytes32(0x88475251bcec25245a44bddd92b2c36db6c9c48bc6d91b5d0da78af3229ff783);
+        bytes32 root = bytes32(0xe81a65c5c0f2a36e40b6872fcfdd62dbb67d47f3d49a6b978c0d4440341e723f);
+        bytes32 leaf = bytes32(0xd85d3181f1178b07e89691aa2bfcd4d88837f011fcda3326b4ce9a68ec6d9e44);
+        assertTrue(SSZ.isValidMerkleProof(leaf, index, branch, root));
+    }
+}
