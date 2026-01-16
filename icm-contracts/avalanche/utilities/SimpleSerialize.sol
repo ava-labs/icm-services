2078eea9915d508173c6e08f693b99ecb1ba17cb
mdelle1 <108158289+mdelle1@users.noreply.github.com>
2026-01-16 07:19:57 -0500

    Add merkleize

--- icm-contracts/avalanche/utilities/SimpleSerialize.sol
+++ icm-contracts/avalanche/utilities/SimpleSerialize.sol
@@ -6,7 +6,46 @@
 // Reference: This is core logic from the Succinct Telepathy Library, which can be found at https://github.com/succinctlabs/telepathy-contracts/blob/main/src/libraries/SimpleSerialize.sol
 pragma solidity ^0.8.30;
 
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+
 library SSZ {
+    /// @notice Merkleizes 32 leaves from calldata using an iterative stack
+    /// @param leaves The leaves data to Merkleize
+    /// @return bytes32 The computed Merkle Root
+    // Requires that the tree is balanced, i.e., the number of leaves is a power of two
+    function merkleize(bytes32[] memory leaves) internal pure returns (bytes32) {
+        // Safety checks
+        uint256 count = leaves.length;
+        require(count > 0, "Leaves must be non-empty");
+        require((count & (count - 1)) == 0, "Leaves must be power of 2");
+        // Allocate the stack to store intermediate nodes
+        uint256 depth = Math.log2(count, Math.Rounding.Floor);
+        bytes32[] memory stack = new bytes32[](depth + 1);
+        uint256 topLevel = 0;
+        // Loop through every leaf
+        for (uint256 i = 0; i < count; i++) {
+            bytes32 node = leaves[i];
+            // Merge up logic. 
+            // If the current index is odd, it means we just finished a right child.
+            // We must hash it with the left child waiting in the stack.
+            uint256 size = i + 1;
+            uint256 level = 0;
+            // While size is even, we are a right sibling, then merge up
+            while ((size % 2) == 0) {
+                node = sha256(bytes.concat(stack[level], node));
+                level++;
+                size /= 2;
+            }
+            // Store the pending node, waiting for its right sibling 
+            stack[level] = node;
+            if (level > topLevel) {
+                topLevel = level;
+            }
+        }
+        // The final root is at the top level
+        return stack[topLevel];
+    }
+
     /// @notice Computes the Merkle Root using a leaf, its index, and the proof
     /// @param leaf The leaf data to verify
     /// @param index The Generalized Index (gIndex) of the leaf
--- icm-contracts/avalanche/utilities/tests/SimpleSerializeTests.t.sol
+++ icm-contracts/avalanche/utilities/tests/SimpleSerializeTests.t.sol
@@ -23,4 +23,27 @@ contract SSZTest is Test {
         bytes32 leaf = bytes32(0xd85d3181f1178b07e89691aa2bfcd4d88837f011fcda3326b4ce9a68ec6d9e44);
         assertTrue(SSZ.isValidMerkleProof(leaf, index, branch, root));
     }
+    function testMerkleizeLeavesBalanced() public pure {
+        // Define leaves
+        bytes32[] memory leaves = new bytes32[](8);
+        leaves[0] = bytes32(0xe424020000000000000000000000000000000000000000000000000000000000);
+        leaves[1] = bytes32(0x75410a8f37f9506fb3f972cce6ece955e381e51037e432ce4ca47479c9cd9158);
+        leaves[2] = bytes32(0xe6af38835c0ac3c2b0d561dfaec168171d7d77c1c2e8e74ff9b1891cf43faf8d);
+        leaves[3] = bytes32(0x3e4fb2d12bd835bc6ee23b5ec65a43f4493e32f5ef45d46bd2c38830b17672bb);
+        leaves[4] = bytes32(0x880548f4df2d4003f7be2fbbde112eb46b8f756b5e33202e04863000e4383f3b);
+        leaves[5] = bytes32(0x88475251bcec25245a44bddd92b2c36db6c9c48bc6d91b5d0da78af3229ff783);
+        leaves[6] = bytes32(0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563);
+        leaves[7] = bytes32(0x5b38da6a701c568545dcfcb03fcb875f56beddc4f7fe24ebd506de91ee3134b2);
+        // Define intermediate nodes
+        bytes32 h01 = sha256(bytes.concat(leaves[0], leaves[1]));
+        bytes32 h23 = sha256(bytes.concat(leaves[2], leaves[3]));
+        bytes32 h45 = sha256(bytes.concat(leaves[4], leaves[5]));
+        bytes32 h67 = sha256(bytes.concat(leaves[6], leaves[7]));
+        bytes32 h0123 = sha256(bytes.concat(h01, h23));
+        bytes32 h4567 = sha256(bytes.concat(h45, h67));
+        // Assert
+        bytes32 expectedRoot = sha256(bytes.concat(h0123, h4567));
+        bytes32 computedRoot = SSZ.merkleize(leaves);
+        assertEq(expectedRoot, computedRoot, "Merkleize mismatch");
+    } 
 }
