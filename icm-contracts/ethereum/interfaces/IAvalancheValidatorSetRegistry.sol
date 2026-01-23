8e73b65b3ff95586ce045bfe159bba6b36c2a531
Jacob Turner <jacob.turner870@gmail.com>
2026-01-23 16:21:59 +0100

    First implementation of registering validator set shards

--- icm-contracts/ethereum/interfaces/IAvalancheValidatorSetRegistry.sol
+++ icm-contracts/ethereum/interfaces/IAvalancheValidatorSetRegistry.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+import {ICMMessage} from "../../common/ICM.sol";
+
 /**
  * @title IAvalancheValidatorSetRegistry
  * @notice Interface for an Avalanche Validator Registry contract
@@ -9,10 +11,23 @@ pragma solidity ^0.8.30;
  */
 interface IAvalancheValidatorSetRegistry {
     event ValidatorSetRegistered(
-        uint256 indexed validatorSetID, bytes32 indexed avalancheBlockchainID
+        bytes32 indexed avalancheBlockchainID
     );
 
     event ValidatorSetUpdated(
-        uint256 indexed validatorSetID, bytes32 indexed avalancheBlockchainID
+        bytes32 indexed avalancheBlockchainID
     );
+
+    /**
+     * @notice Registers a new validator set
+     * @dev A validator set must can be registered by anyone, but it must be signed by the current P-chain validator set
+     * known to this contract. Updates to any validator set after registration are always authenticated by a
+     * signed ICM message from the current validator set, giving P-chain validator sets no elevated permission after
+     * registration.
+     * @param message The ICM message containing the validator set to register.
+     */
+    function registerValidatorSet(
+        ICMMessage calldata message,
+        bytes memory validatorBytes
+    ) external;
 }
