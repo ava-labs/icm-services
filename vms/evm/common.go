5cbb45789007a51e5ee18d68ec7daffcd2f4973b
Jacob Turner <jacob.turner870@gmail.com>
2026-02-20 11:47:59 +0100

    Reworked go generate command to avoid need to split files while generating mocks

--- vms/evm/common.go
+++ vms/evm/common.go
@@ -1,6 +1,7 @@
 // Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
-
+//
+//go:generate go run go.uber.org/mock/mockgen -destination=./mocks/mock_destination_rpc_client.go -package=mocks github.com/ava-labs/icm-services/vms/evm DestinationRPCClient
 package evm
 
 import (
@@ -16,6 +17,7 @@ import (
 	avalancheWarp "github.com/ava-labs/avalanchego/vms/platformvm/warp"
 	"github.com/ava-labs/icm-services/utils"
 	"github.com/ava-labs/icm-services/vms/evm/signer"
+	ethereum "github.com/ava-labs/libevm"
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/core/types"
 	"go.uber.org/zap"
@@ -33,6 +35,20 @@ type CommonDestinationClient interface {
 	ConcurrentSigners() []*readonlyConcurrentSigner
 	AccessList(data txData) types.AccessList
 	TxInclusionTimeout() time.Duration
+}
+
+// DestionationRPCClient interface represents the minimal interface needed for querying RPC endpoints.
+type DestinationRPCClient interface {
+	BlockByNumber(ctx context.Context, blockNumber *big.Int) (*types.Block, error)
+	ChainID(ctx context.Context) (*big.Int, error)
+	NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (uint64, error)
+	SuggestGasTipCap(ctx context.Context) (*big.Int, error)
+	SendTransaction(ctx context.Context, tx *types.Transaction) error
+	TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error)
+	BlockNumber(ctx context.Context) (uint64, error)
+	CallContract(ctx context.Context, msg ethereum.CallMsg, blockNumber *big.Int) ([]byte, error)
+
+	EstimateBaseFee(ctx context.Context) (*big.Int, error)
 }
 
 type txData struct {
