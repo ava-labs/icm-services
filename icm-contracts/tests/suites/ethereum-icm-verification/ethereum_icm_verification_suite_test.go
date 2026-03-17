b2665bca638c8ab4828ab2372eb32dc39fcba9be
Jacob Turner <jacob.turner870@gmail.com>
2026-03-17 10:50:40 +0100

    update abi bindings

--- icm-contracts/tests/suites/ethereum-icm-verification/ethereum_icm_verification_suite_test.go
+++ icm-contracts/tests/suites/ethereum-icm-verification/ethereum_icm_verification_suite_test.go
@@ -106,13 +106,4 @@ var _ = ginkgo.Describe("[Ethereum ICM Verification integration tests]", func() 
 				localEthereumNetworkInstance,
 			)
 		})
-	ginkgo.It("Test AvalancheValidatorSetRegistry",
-		ginkgo.Label(ethereumICMVerificationLabel),
-		func(ctx context.Context) {
-			ethereumIcmVerification.AvalancheValidatorSetRegistry(
-				ctx,
-				localAvalancheNetworkInstance,
-				localEthereumNetworkInstance,
-			)
-		})
 })
