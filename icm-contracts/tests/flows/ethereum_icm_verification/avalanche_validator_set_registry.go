f6c697156459e376d646ef9478f703d8f801f434
Jacob Turner <jacob.turner870@gmail.com>
2026-03-27 12:07:06 +0100

    Fleshing out the validator set registry e2e test to use teleportermessenger and adapters

--- icm-contracts/tests/flows/ethereum_icm_verification/avalanche_validator_set_registry.go
+++ icm-contracts/tests/flows/ethereum_icm_verification/avalanche_validator_set_registry.go
@@ -2,12 +2,19 @@ package ethereum_icm_verification
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/rand"
 
 	"github.com/ava-labs/avalanchego/vms/platformvm"
+	adapter "github.com/ava-labs/icm-services/abi-bindings/go/Adapter"
 	diffupdater "github.com/ava-labs/icm-services/abi-bindings/go/DiffUpdater"
+	ecdsaverifier "github.com/ava-labs/icm-services/abi-bindings/go/mocks/ECDSAVerifier"
 	localnetwork "github.com/ava-labs/icm-services/icm-contracts/tests/network"
 	"github.com/ava-labs/icm-services/icm-contracts/tests/utils"
+	deploymentUtils "github.com/ava-labs/icm-services/icm-contracts/utils/deployment-utils"
 	"github.com/ava-labs/libevm/accounts/abi/bind"
+	"github.com/ava-labs/libevm/common"
+	"github.com/ava-labs/libevm/crypto"
 	. "github.com/onsi/gomega"
 )
 
@@ -19,10 +26,25 @@ func AvalancheValidatorSetRegistry(
 	ctx context.Context,
 	localAvalancheNetwork *localnetwork.LocalAvalancheNetwork,
 	localEthereumNetwork *localnetwork.LocalEthereumNetwork,
+	ecdsaVerifierByteCodeFile string,
+	adapterByteCodeFile string,
+	teleporterInfo utils.TeleporterTestInfo,
 ) {
+	// set top-level variables
 	_, fundedEthereumKey := localEthereumNetwork.GetFundedAccountInfo()
 	primaryNetworkInfo := localAvalancheNetwork.GetPrimaryNetworkInfo()
-	contractAddress, serializedShards := utils.DeployDiffUpdater(
+	_, fundedAvalancheKey := localAvalancheNetwork.GetFundedAccountInfo()
+	ethereumNetworkInfo := localEthereumNetwork.GetNetworkInfo()[0]
+	// Get a private key to sign messages from Ethereum
+	ecdsaSigner, err := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
+	Expect(err).Should(BeNil())
+
+	// =========================================================================
+	// Step 1: Deploy the DiffUpdater contract on both chains
+	// =========================================================================
+
+	// first deploy the DiffUpdater contract on the Ethereum network and initialize it
+	registryContractAddress, serializedShards := utils.DeployDiffUpdater(
 		ctx,
 		localEthereumNetwork.EthereumTestInfo(),
 		fundedEthereumKey,
@@ -36,7 +58,7 @@ func AvalancheValidatorSetRegistry(
 	Expect(len(serializedShards)).Should(Equal(4))
 
 	avalancheValidatorSetRegistry, err := diffupdater.NewDiffUpdater(
-		contractAddress,
+		registryContractAddress,
 		localEthereumNetwork.EthClient,
 	)
 	Expect(err).Should(BeNil())
@@ -62,4 +84,151 @@ func AvalancheValidatorSetRegistry(
 	registered, err := avalancheValidatorSetRegistry.IsRegistered(&bind.CallOpts{}, primaryNetworkInfo.BlockchainID)
 	Expect(err).Should(BeNil())
 	Expect(registered).Should(BeTrue())
+
+	// Deploy the DiffUpdater contract on Avalanche
+	// N.B. We don't need to initialize the contract as it will only be used for sending messages
+	contractAddress, _ := utils.DeployDiffUpdater(
+		ctx,
+		&primaryNetworkInfo,
+		fundedAvalancheKey,
+		1,
+		primaryNetworkInfo.BlockchainID,
+		primaryNetworkInfo.SubnetID,
+		platformvm.NewClient(primaryNetworkInfo.NodeURIs[0]),
+		// N.B. This must be the same as above so that the constructor arguments match
+		// for both deployments
+		5,
+	)
+	// Ensure that the contract address is the same as the one deployed on Ethereum
+	Expect(contractAddress).Should(Equal(registryContractAddress))
+
+	// =========================================================================
+	// Step 2: Deploy the ECDSA verifier contract on both chains
+	// =========================================================================
+
+	byteCode, err := deploymentUtils.ExtractByteCodeFromFile(ecdsaVerifierByteCodeFile)
+	Expect(err).Should(BeNil())
+
+	// Generate the ECDSAVerifier deployer transaction via Nick's method
+	ecdsaVerifierContractTransaction,
+		ecdsaVerifierDeployerAddress,
+		ecdsaVerifierContractAddress,
+		err := deploymentUtils.ConstructKeylessTransaction(
+		byteCode,
+		nil,
+		deploymentUtils.GetDefaultContractCreationGasPrice(),
+		nil,
+	)
+	Expect(err).Should(BeNil())
+	// Deploy the ECDSAVerifier contract on the C-Chain
+	utils.DeployWithNicksMethod(
+		ctx,
+		&primaryNetworkInfo,
+		ecdsaVerifierContractTransaction,
+		ecdsaVerifierDeployerAddress,
+		ecdsaVerifierContractAddress,
+		fundedAvalancheKey,
+	)
+
+	// Initialize the ECDSAVerifier contract on the C-Chain with the `ecdsaSigner` address
+	avalancheEcdsaVerifier, err := ecdsaverifier.NewECDSAVerifier(
+		ecdsaVerifierContractAddress,
+		primaryNetworkInfo.EthClient,
+	)
+	Expect(err).Should(BeNil())
+	opts, err = bind.NewKeyedTransactorWithChainID(fundedAvalancheKey, primaryNetworkInfo.EVMChainID)
+	Expect(err).Should(BeNil())
+	tx, err := avalancheEcdsaVerifier.Initialize(opts, crypto.PubkeyToAddress(ecdsaSigner.PublicKey))
+	Expect(err).Should(BeNil())
+	// Wait for the transaction to be accepted
+	utils.WaitForTransactionSuccess(ctx, primaryNetworkInfo.EthClient, tx.Hash())
+
+	// Deploy the ECDSAVerifier contract on the Ethereum chain
+	utils.DeployWithNicksMethod(
+		ctx,
+		localEthereumNetwork.EthereumTestInfo(),
+		ecdsaVerifierContractTransaction,
+		ecdsaVerifierDeployerAddress,
+		ecdsaVerifierContractAddress,
+		fundedEthereumKey,
+	)
+	// Initialize the ECDSAVerifier contract on Ethereum with the `ecdsaSigner` address
+	ethereumEcdsaVerifier, err := ecdsaverifier.NewECDSAVerifier(
+		ecdsaVerifierContractAddress,
+		localEthereumNetwork.EthClient,
+	)
+	Expect(err).Should(BeNil())
+	opts, err = bind.NewKeyedTransactorWithChainID(fundedEthereumKey, localEthereumNetwork.ChainID)
+	Expect(err).Should(BeNil())
+	tx, err = ethereumEcdsaVerifier.Initialize(opts, crypto.PubkeyToAddress(ecdsaSigner.PublicKey))
+	Expect(err).Should(BeNil())
+	// Wait for the transaction to be accepted
+	utils.WaitForTransactionSuccess(ctx, localEthereumNetwork.EthClient, tx.Hash())
+
+	// =========================================================================
+	// Step 3: Deploy the Adapter contract on both chains
+	// =========================================================================
+	byteCode, err = deploymentUtils.ExtractByteCodeFromFile(adapterByteCodeFile)
+	Expect(err).Should(BeNil())
+
+	// Generate the Adapter deployer transaction via Nick's method
+	adapterContractTransaction,
+		adapterDeployerAddress,
+		adapterContractAddress,
+		err := deploymentUtils.ConstructKeylessTransaction(
+		byteCode,
+		nil,
+		deploymentUtils.GetDefaultContractCreationGasPrice(),
+		nil,
+	)
+	Expect(err).Should(BeNil())
+	adapterABI, err := adapter.AdapterMetaData.GetAbi()
+	Expect(err).Should(BeNil())
+	byteCode, err = deploymentUtils.AddConstructorArgsToByteCode(
+		adapterABI,
+		byteCode,
+		1,
+		primaryNetworkInfo.BlockchainID,
+		ethereumNetworkInfo.ChainID(),
+		ecdsaVerifierContractAddress,
+		registryContractAddress,
+	)
+	// Deploy the Adapter contract on the C-Chain
+	utils.DeployWithNicksMethod(
+		ctx,
+		&primaryNetworkInfo,
+		adapterContractTransaction,
+		adapterDeployerAddress,
+		adapterContractAddress,
+		fundedAvalancheKey,
+	)
+	// Deploy the Adapter contract on Ethereum
+	utils.DeployWithNicksMethod(
+		ctx,
+		ethereumNetworkInfo,
+		adapterContractTransaction,
+		adapterDeployerAddress,
+		adapterContractAddress,
+		fundedEthereumKey,
+	)
+
+	// =========================================================================
+	// Step 4: Deploy the TeleporterMessengerV2 contract on both chains
+	// =========================================================================
+
+	var teleporterContractAddress common.Address
+	// Deploy the Teleporter registry contracts to all subnets and the C-Chain.
+	for _, l1 := range localAvalancheNetwork.GetAllL1Infos() {
+		teleporterContractAddress = utils.DeployTeleporterV2(ctx, &l1, adapterContractAddress, fundedAvalancheKey)
+		teleporterInfo.SetTeleporterV2(teleporterContractAddress, l1.BlockchainID)
+	}
+	// Deploy the Teleporter registry contracts to the Ethereum network.
+	teleporterContractAddress = utils.DeployTeleporterV2(
+		ctx,
+		localEthereumNetwork.EthereumTestInfo(),
+		adapterContractAddress,
+		fundedEthereumKey,
+	)
+	teleporterInfo.SetTeleporterV2(teleporterContractAddress, localEthereumNetwork.EthereumTestInfo().ChainID())
+
 }
